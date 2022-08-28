dfd31a6752abe9b3b46a3350f78485b47bcffe98
ooothinhooo <74719505+ooothinhooo@users.noreply.github.com>
2022-08-29 00:57:59 +0700

    update header

--- src/utils/data.js
+++ src/utils/data.js
@@ -7,29 +7,29 @@ const heroData = [
         imageURL:
             'https://firebasestorage.googleapis.com/v0/b/fooddeliveryapp154.appspot.com/o/Images%2F1661701225198-ct239_sudoku.png?alt=media&token=a9bcb886-0ca3-4f6f-971f-f2a71ed2ce1a',
     },
-    // {
-    //     id: 2,
-    //     title: 'DIJKSTRA Tìm đường đi ngắn nhất',
-    //     decp: 'CT239',
-    //     price: '550',
-    //     imageURL:
-    //         'https://firebasestorage.googleapis.com/v0/b/fooddeliveryapp154.appspot.com/o/Images%2F1661701244089-ct239_dijstra.png?alt=media&token=6b3821de-ebd5-4a6b-99ab-d8a4a7ac862f',
-    // },
-    // {
-    //     id: 3,
-    //     title: 'Project CT178',
-    //     decp: 'CT178',
-    //     price: '140',
-    //     imageURL:
-    //         'https://firebasestorage.googleapis.com/v0/b/fooddeliveryapp154.appspot.com/o/Images%2F1661700474511-project_ct178.png.png?alt=media&token=2fe892c2-4dc1-47f8-ae4a-5e7f083e0d2b',
-    // },
-    // {
-    //     id: 4,
-    //     title: 'Fish Kebab',
-    //     decp: 'Fish ',
-    //     price: '9.25',
-    //     imageURL: R1,
-    // },
+    {
+        id: 2,
+        title: 'DIJKSTRA Tìm đường đi ngắn nhất',
+        decp: 'CT239',
+        price: '550',
+        imageURL:
+            'https://firebasestorage.googleapis.com/v0/b/fooddeliveryapp154.appspot.com/o/Images%2F1661701244089-ct239_dijstra.png?alt=media&token=6b3821de-ebd5-4a6b-99ab-d8a4a7ac862f',
+    },
+    {
+        id: 3,
+        title: 'Project CT178',
+        decp: 'CT178',
+        price: '140',
+        imageURL:
+            'https://firebasestorage.googleapis.com/v0/b/fooddeliveryapp154.appspot.com/o/Images%2F1661700474511-project_ct178.png.png?alt=media&token=2fe892c2-4dc1-47f8-ae4a-5e7f083e0d2b',
+    },
+    {
+        id: 4,
+        title: 'Fish Kebab',
+        decp: 'Fish ',
+        price: '9.25',
+        imageURL: R1,
+    },
 ];
 
 export default heroData;
