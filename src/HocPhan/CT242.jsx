ac342cc96d00a51ab30cd9d5123d109f950379cf
ooothinhooo <74719505+ooothinhooo@users.noreply.github.com>
2022-09-10 21:48:25 +0700

    update view pdf

--- src/HocPhan/CT242.jsx
+++ src/HocPhan/CT242.jsx
@@ -4,9 +4,21 @@ function CT242() {
    
     return (
         <div className="w-full h-full ">
+            <div className="text-md text-white">
+                Hoặc truy cập tại{' '}
+                <a
+                    href="https://hocphan-ocit.vercel.app/src/html/CT242/CT242.html"
+                    className="text-blue-500"
+                    target="_blank"
+                >
+                    Get
+                </a>
+            </div>
             <iframe
                 src="https://hocphan-ocit.vercel.app/src/html/CT242/CT242.html"
-                className="w-full h-screen  aspect-square bg-gray-300 "
+                className="w-full h-screen  md:aspect-square bg-gray-300 "
+                frameborder="0"
+                allowfullscreen
             ></iframe>
         </div>
     );
