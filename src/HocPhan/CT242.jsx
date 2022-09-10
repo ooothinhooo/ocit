4d7ad77f234e6f99d4dab05c645db946f82df48e
ooothinhooo <74719505+ooothinhooo@users.noreply.github.com>
2022-09-10 21:10:53 +0700

    update pdf view

--- src/HocPhan/CT242.jsx
+++ src/HocPhan/CT242.jsx
@@ -6,7 +6,7 @@ function CT242() {
         <div className="w-full h-full ">
             <iframe
                 src="https://hocphan-ocit.vercel.app/src/html/CT242/CT242.html"
-                className="w-full h-screen  md:aspect-square bg-gray-300 "
+                className="w-full h-screen  aspect-square bg-gray-300 "
             ></iframe>
         </div>
     );
